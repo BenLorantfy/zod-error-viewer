e83c6c9cde026fbd8d6d3ecf2897c7b79e6f3371
Ben <BenLorantfy@users.noreply.github.com>
2024-06-09 11:49:47 -0400

    Fix import

--- packages/zod-error-viewer/.storybook/manager.ts
+++ packages/zod-error-viewer/.storybook/manager.ts
@@ -1,13 +1,9 @@
 import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming/create";
 
-// @ts-expect-error
-import logo from "../img/logo-wide.png";
-
 const theme = create({
   base: "light",
   brandTitle: "zod-error-viewer",
-  brandImage: logo,
 });
 
 addons.setConfig({
